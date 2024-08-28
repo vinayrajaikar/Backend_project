74259c127914bd97b210e2fbe738c9f1f6f203f1
Vinayrajaikar <vinayrajaikar@gmail.com>
2024-08-28 11:50:06 +0530

    user_route setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).
         catch((err)=> next(err))
     }
@@ -7,6 +7,12 @@ const asyncHandler = (requestHandler)=>{
 
 
 export {asyncHandler} 
+
+
+
+
+
+
 
 // Wrapper function using try catch
 // const asyncHandler = (fn) => {
