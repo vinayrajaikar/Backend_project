f33664f40eca9dc990a50fbec2b9af99e2dc08ae
Vinayrajaikar <vinayrajaikar@gmail.com>
2024-08-31 17:55:31 +0530

    Fixed refresh token validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -188,7 +188,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 try {
         const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken
     
-        if(incomingRefreshToken){
+        if(!incomingRefreshToken){
             throw new ApiError(401, "unauthorized request")
         }
     
